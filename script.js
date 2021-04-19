ad8b0acc638b3bcdf23a58d0d22eaf0e4bf87b41
maria_shchipilo <schipilo1994@mail.ru>
2021-04-19 21:14:32 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,83 @@
+// 1 задание
+// let btn = document.querySelector('.btn');
+
+// function buttonClick(){
+//     let input=document.getElementById('exc');
+//     alert('Привет')
+// } 
+// btn.addEventListener("click",buttonClick);
+
+
+
+// 2 задание
+// let btn = document.querySelector('.btn');
+
+// function buttonClick(){
+//     let input=document.getElementById('exc');
+//     alert('Привет')
+// } 
+// btn.addEventListener("click",buttonClick);
+
+
+
+// 4 задание
+// let btn = document.querySelector('.btn');
+
+// function buttonClick(){
+//     let input=document.getElementById('exc');
+//     let n = input.value;
+//     alert('Ку-ку')
+// } 
+// btn.addEventListener("click",buttonClick);
+
+
+
+// 5 задание
+// let btn = document.querySelector('.btn');
+
+// function buttonClick(){
+// let input=document.getElementById('excOne');
+// input.value = 'Ой, я поменял свой текст!';
+// } 
+// btn.addEventListener("click",buttonClick);
+
+
+
+// 6задание не сделала
+// let btn = document.querySelector('.btn');
+// function buttonClick(){
+//     let input = document.getElementById('monkey');
+//     input.value ="<img src='D:\JS\ДЗ\19.04.2021\img\2.jpg'>";
+// }
+// btn.addEventListener("click",buttonClick);
+
+
+
+// 7 задание
+// let btn = document.querySelector('.btn');
+// function buttonClick(elem) {
+//     elem.value = 'Нажми на меня';
+// }
+// btn.addEventListener("click",buttonClick);
+
+
+// 21 задание
+// let btn = document.querySelector('.btn');
+// function buttonClick() {
+//     let inputOne = document.getElementById('hll');
+//     let inputTwo = document.getElementById('gdb');
+//     let strOne = inputOne.value;
+//     let strTwo = inputTwo.value;
+//     inputOne.value = strTwo;
+//     inputTwo.value = strOne;
+// }
+// btn.addEventListener("click",buttonClick);
+
+// 22 задание
+let btn = document.querySelector('.btn');
+function buttonClick() {
+    let input = document.getElementById('hll');
+    let num = input.value;
+    alert(Math.pow(num,2));
+}
+btn.addEventListener("click",buttonClick);
